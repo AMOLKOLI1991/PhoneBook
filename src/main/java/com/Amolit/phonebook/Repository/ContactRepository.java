8e30c442228d2e80b594e7f1abff929acd23f9a7
AMOLKOLI1991 <amolkoli595@gmail.com>
2021-11-10 23:25:55 +0530

    PHON-:implements service class

--- src/main/java/com/Amolit/phonebook/Repository/ContactRepository.java
+++ src/main/java/com/Amolit/phonebook/Repository/ContactRepository.java
@@ -3,9 +3,11 @@ package com.Amolit.phonebook.Repository;
 import java.io.Serializable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Amolit.phonebook.entity.Contact;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Serializable>{
 
 }
--- src/main/java/com/Amolit/phonebook/service/ContactServiceImpl.java
+++ src/main/java/com/Amolit/phonebook/service/ContactServiceImpl.java
@@ -1,5 +1,53 @@
 package com.Amolit.phonebook.service;
 
-public class ContactServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Amolit.phonebook.Repository.ContactRepository;
+import com.Amolit.phonebook.entity.Contact;
+
+@Service
+public class ContactServiceImpl implements ContactServiceI{
+
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	@Override
+	public boolean saveContact(Contact contact) {
+	Contact save=	contactRepository.save(contact);
+	if(save !=null )  // && save.getContactId() != null
+	{
+		return true;
+	}
+		return false;
+	}
+
+	@Override
+	public List<Contact> getAllContact() {
+	List<Contact> findAll=	contactRepository.findAll();
+		return findAll;
+	}
+
+	@Override
+	public Contact getContactById(int contactId) {
+	Optional<Contact> findById=	contactRepository.findById(contactId);
+	if(findById.isPresent()) {
+		Contact contact = findById.get();
+		return contact;
+	}
+		return null;
+	}
+
+	@Override
+	public boolean deleteContactById(int contactId) {
+		boolean status =contactRepository.existsById(contactId);
+		if(status) {
+			contactRepository.deleteById(contactId);
+		}
+		return false;
+	}
 
 }
